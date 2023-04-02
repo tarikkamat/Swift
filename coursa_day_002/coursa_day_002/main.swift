5e179acadef4306eaf029461bce3017ac83cc225
Tarık KAMAT <tarikkamat2@gmail.com>
2023-04-02 21:48:29 +0300

    print

---
+++ coursa_day_002/coursa_day_002/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  coursa_day_002
+//
+//  Created by Tarık KAMAT on 4/2/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+// Creating a variable without an initial value
+var number:Int?
+number = 20
+
+// "!" We should use it to guarantee that the variable is not null.
+print("Number: \(number!)")
+
+// Define multiple variables from a single line.
+var num = 10, numTwo = 30, text = "Hello, world"
+print("text: \(text)")
+
+var sum = num + numTwo
+print("Sum: \(sum)")
+
